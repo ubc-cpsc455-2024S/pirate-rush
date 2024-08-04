c18f029e112f236f32ca3da956b2f6e78654f81f
jacksonliiii <jacksonli2001@gmail.com>
2024-08-03 18:05:06 -0700

    Add LeaderboardPage.jsx

---
+++ crew-builder-v2/frontend/src/pages/LeaderboardPage.jsx
@@ -0,0 +1,11 @@
+function LeaderboardPage() {
+  return (
+    <>
+      <div className="leaderboard-container">
+        <h1 className="mulish-heading">Leaderboard</h1>
+      </div>
+    </>
+  );
+}
+
+export default LeaderboardPage;
